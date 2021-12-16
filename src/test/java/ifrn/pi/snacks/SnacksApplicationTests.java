8ddc972434061aa0c12df21a20a36ae78d8c8a68
Marcílio-Filho-Create <marciliomtfilho6@gmail.com>
2021-12-16 16:55:23 -0300

    Initial commit

---
+++ src/test/java/ifrn/pi/snacks/SnacksApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.pi.snacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
