bcd9272a5a949f41abd86dc4f2ae8eda6cd40b77
Marcílio-Filho-Create <marciliomtfilho6@gmail.com>
2021-12-22 19:55:23 -0300

    Entidade Usuário e Controller de Snacks
    Nesse push criei a entidade usuário e o controller principal da aplicação.

---
+++ src/main/java/ifrn/pi/snacks/controllers/SnacksController.java
@@ -0,0 +1,8 @@
+package ifrn.pi.snacks.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SnacksController {
+
+}
---
+++ src/main/java/ifrn/pi/snacks/models/Usuario.java
@@ -0,0 +1,51 @@
+package ifrn.pi.snacks.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private char tipo;
+	private String email;
+	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public char getTipo() {
+		return tipo;
+	}
+	public void setTipo(char tipo) {
+		this.tipo = tipo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
